78d2d305fe8720a36ea777d1ec1beb356b6b7dc4
NerdHerd91 <sventrel@outlook.com>
2014-02-03 16:16:28 -0800

    DId stuff

--- Collaborative.java
+++ Collaborative.java
@@ -9,6 +9,8 @@ public class Collaborative {
 		// Maps that maps a user id to a subset of ratings given by that user.
 		Map<Integer, Map<Integer, Rating>> trainRatings = new HashMap<Integer, Map<Integer, Rating>>();
 		Map<Integer, Map<Integer, Rating>> testRatings = new HashMap<Integer, Map<Integer, Rating>>();
+		// Maps that map a user id to their mean.
+		Map<Integer, Double> testMean = new HashMap<Integer, Double>();
 
 		// Parse the data from the files in ./DataSet directory
 		try {
@@ -26,21 +28,25 @@ public class Collaborative {
 			e.printStackTrace();
 		}
 
+		// Fill Map of means.
+		for (Integer uid : trainRatings.keySet()) {
+			testMean.put(uid, calculateMean(new HashSet<Rating>(trainRatings.get(uid).values())));
+		}
+
 		int k = 1;
-		int matches = 0;
+		int total = 0;
+		double errorSum = 0;
 		// Predict ratings for users.
 		for (Integer uid : testRatings.keySet()) {
 			for (Integer mid : testRatings.get(uid).keySet()) {
 				Rating r = testRatings.get(uid).get(mid);
-				double pred = calculateWeightedSum(trainRatings, new HashSet<Rating>(trainRatings.get(uid).values()), mid, k);
-				if (r.getRating() == pred) {
-					matches++;
-				}
+				errorSum += Math.abs(r.getRating() - calculateWeightedSum(trainRatings, new HashSet<Rating>(trainRatings.get(uid).values()), testMean, uid,  mid, k));
+				total++;
 			}
 		}
 
 		// Compute accuracy of algorithm.
-		System.out.println("Matches: " + matches);
+		System.out.printf("Percent Error: %.2f%%\n", (errorSum / total * 100));
 	}
 
 	/**
@@ -73,20 +79,22 @@ public class Collaborative {
 	*
 	* @param train Map from userId to Map of ratings for that user.
 	* @param test Set of ratings for the active user.
+	* @param means Map from uid to mean value.
+	* @param uidA Integer user id.
 	* @param mid Integer movie id.
 	* @param k Integer normalization factor.
 	* @return Double indicated weighted sum.
 	*/
-	public static double calculateWeightedSum(Map<Integer, Map<Integer, Rating>> train, Set<Rating> test, int mid, int k) {
-		double meanTest = calculateMean(test);
+	public static double calculateWeightedSum(Map<Integer, Map<Integer, Rating>> train, Set<Rating> test, Map<Integer, Double> means, int uidA, int mid, int k) {
 		double sum = 0;
 		for (Integer uid : train.keySet()) {
-			Set<Rating> ratings = new HashSet<Rating>(train.get(uid).values());
-			if (train.get(uid).containsKey(mid)) {
-				sum += calculateWeight(train.get(uid), test) * (train.get(uid).get(mid).getRating() - calculateMean(ratings));
+			Map<Integer, Rating> user = train.get(uid);
+			Set<Rating> ratings = new HashSet<Rating>(user.values());
+			if (user.containsKey(mid)) {
+				sum += calculateWeight(user, test, means, uidA, uid) * (user.get(mid).getRating() - means.get(uid));
 			}
 		}
-		return meanTest + k * sum;
+		return means.get(uidA) + k * sum;
 	}
 
 	/**
@@ -94,20 +102,21 @@ public class Collaborative {
 	*
 	* @param train Map from mid to Rating for a particular user (i).
 	* @param test Set of Ratings for a particular active user.
+	* @param means Map from uid to mean.
+	* @param uid1 Integer user id for test.
+	* @param uid2 Integer user id for train
 	* @return Returns a Double representing the computed weight.
 	*/
-	public static double calculateWeight(Map<Integer, Rating> train, Set<Rating> test) {
+	public static double calculateWeight(Map<Integer, Rating> train, Set<Rating> test, Map<Integer, Double>means, int uid1, int uid2) {
 		double numSum = 0;
 		double denTestSum = 0;
 		double denTrainSum = 0;
-		double meanTest = calculateMean(test);
-		double meanTrain = calculateMean(new HashSet<Rating>(train.values()));
 
 		for (Rating r : test) {
 			if (train.containsKey(r.getMovieId())) {
-				numSum += (r.getRating() - meanTest) * (train.get(r.getMovieId()).getRating() - meanTrain);
-				denTestSum += Math.pow((r.getRating() - meanTest), 2);
-				denTrainSum += Math.pow((train.get(r.getMovieId()).getRating() - meanTrain), 2);
+				numSum += (r.getRating() - means.get(uid1)) * (train.get(r.getMovieId()).getRating() - means.get(uid2));
+				denTestSum += Math.pow((r.getRating() - means.get(uid1)), 2);
+				denTrainSum += Math.pow((train.get(r.getMovieId()).getRating() - means.get(uid2)), 2);
 			}
 		}
 		return numSum / Math.sqrt(denTestSum * denTrainSum);
@@ -125,21 +134,5 @@ public class Collaborative {
 			sum += r.getRating();
 		}
 		return sum / ratings.size();
-	}
-
-	/**
-	* Determines if a set of ratings contains a certain movie.
-	*
-	* @param mid Integer movie id.
-	* @param ratings Set of ratings for a particular user.
-	* @return Boolean indicating if movie was found in the set.
-	*/
-	private static boolean containsMovie(int mid, Set<Rating> ratings) {
-		for (Rating r : ratings) {
-			if (r.getMovieId() == mid) {
-				return true;
-			}
-		}
-		return false;
 	}
 }
